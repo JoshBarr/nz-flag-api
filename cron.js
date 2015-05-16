8002a89a51a7169c61491823f00e84e14b46b363
Josh Barr <josh@joshbarr.com>
2015-05-16 17:58:49 +1200

    cron now points to the right task

--- cron.js
+++ cron.js
@@ -4,24 +4,25 @@ var spawn = require('child_process').spawn;
 
 function scrapeGovtNZSite() {
     console.log('Starting scrape', Date.now());
-    var child = spawn('npm run dump && npm run scrape');
+    var child = spawn('python', ['./flag/scrape.py', 'scrape']);
 
     child.stdout.on('data', function(data) {
-        console.log(data);
+        // console.log(data);
     });
 
     child.stderr.on('data', function(data) {
-        console.log(data);
+        // console.log(data);
     });
 
     child.on('close', function(code) {
         console.log(code);
     });
+
+    child.on('error', function( err ){ throw err })
 }
 
 
 var j = schedule.scheduleJob('* 1 * * *', function(){
-    console.log('The answer to life, the universe, and everything!');
     scrapeGovtNZSite()
 });
 
